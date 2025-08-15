34f577e3960e68ed368e26145867ec962cae7f5c
jaya-61 <jayalakshmigujarathi12@gmail.com>
2025-08-15 19:57:51 +0530

    Create caption_generator.py

---
+++ caption_generator.py
@@ -0,0 +1,17 @@
+def predict_step(image_paths):
+  images=[]
+  for image_path in image_paths:
+    i_image=Image.open(image_path)
+    if i_image.mode !="RGB":
+      i_image=i_image.convert(mode="RGB")
+
+    images.append(i_image)
+
+  pixel_values=feature_extracter(images=images, return_tensors="pt").pixel_values
+  pixel_values=pixel_values.to(device)
+
+  output_ids=model.generate(pixel_values, **gen_kwargs)
+
+  preds=tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+  preds=[pred.strip() for pred in preds]
+  return preds
