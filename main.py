adf190b0d282fb51e766e306a23b92fed7915c2c
jaya-61 <jayalakshmigujarathi12@gmail.com>
2025-08-15 20:50:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import first
 import configure_genrator
 import caption_generator
-import image
+from first import predict_step
+
+# Call the function
+predict_step(['/content/drive/MyDrive/images/Plane-flying-on-earth-atmosphere.jpg'])
+
+
