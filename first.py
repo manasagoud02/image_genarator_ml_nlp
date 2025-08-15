ccb1a2b0ae18146a023c3d184997ee3e15382b12
jaya-61 <jayalakshmigujarathi12@gmail.com>
2025-08-15 20:23:05 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,3 @@
-from transformers import VisionEnocderDecoderModel, ViTImageProcessor, AutoTokenizer
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
 from PIL import Image
